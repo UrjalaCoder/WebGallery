9f69067f6f2bcddb7c53047fae2a14e3a304a6e2
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2020-03-13 17:43:49 +0200

    Started firebase integration

--- src-server/api.ts
+++ src-server/api.ts
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch'
+import * as admin from 'firebase-admin'
+import { LoginCredentials } from '../common/constants'
 
-export function loginUser(params: { username: string; password: string }) {
+export function loginUser(params: LoginCredentials): void {
 	const { username, password } = params
 }
